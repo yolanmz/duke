8b0693c6a184ec41eb46e280ba694f44ec3c243d
Yolanda <yolanda993322@gmail.com>
2020-01-23 00:47:39 -0500

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
