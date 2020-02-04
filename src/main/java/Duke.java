4ca6afdc10cbe2449f47f11c2c0839b52edead76
Yolanda <yolanda993322@gmail.com>
2020-02-03 22:02:51 -0500

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,8 +8,40 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("------------------------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------");
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        String[] list = new String[100];
+        int listCounter = 0;
+        while(!input.equals("bye")){
+            if(input.equals("list")){
+                System.out.println("------------------------------------------------");
+                System.out.println("list");
+                for(int i=0; i<listCounter;i++){
+                    System.out.println(i+1 + ". " +  list[i]);
+                }
+                System.out.println("------------------------------------------------");
+            } else{
+                list[listCounter] = input;
+                listCounter++;
+                System.out.println("------------------------------------------------");
+                System.out.println("added: " + input);
+                System.out.println("------------------------------------------------");
+
+            }
+            in = new Scanner(System.in);
+            input = in.nextLine();
+
+        }
+        if(input.equals("bye")) {
+            System.out.println("------------------------------------------------");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("------------------------------------------------");
+        }
+
     }
 }
