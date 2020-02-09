39a75c4789e6ea99f16a158467ea5dccb17967a2
Yolanda <yolanda993322@gmail.com>
2020-02-09 06:01:34 -0500

    Code Formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,47 +1,53 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void printSeparator() {
+        System.out.println("------------------------------------------------");
+    }
+    public static void printWelcomeMessage(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("------------------------------------------------");
+        printSeparator();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println("------------------------------------------------");
+        printSeparator();
 
+    }
+    public static void main(String[] args) {
+        printWelcomeMessage();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         String[] list = new String[100];
         int listCounter = 0;
         while(!input.equals("bye")){
             if(input.equals("list")){
-                System.out.println("------------------------------------------------");
+                printSeparator();
                 System.out.println("list");
                 for(int i=0; i<listCounter;i++){
                     System.out.println(i+1 + ". " +  list[i]);
                 }
-                System.out.println("------------------------------------------------");
+                printSeparator();
             } else{
                 list[listCounter] = input;
                 listCounter++;
-                System.out.println("------------------------------------------------");
+                printSeparator();
                 System.out.println("added: " + input);
-                System.out.println("------------------------------------------------");
+                printSeparator();
 
             }
             in = new Scanner(System.in);
             input = in.nextLine();
 
         }
-        if(input.equals("bye")) {
-            System.out.println("------------------------------------------------");
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("------------------------------------------------");
-        }
+
+        printSeparator();
+        System.out.println("Bye. Hope to see you again soon!");
+        printSeparator();
+
 
     }
 }
